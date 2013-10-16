154a5361cd153121aac3e278f68afada7ac89fe6
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2013-10-16 12:50:06 +0200

    usb: musb: core: properly free host / device structs in err path
    BugLink: http://bugs.launchpad.net/bugs/1265614
    
    commit 0d2dd7eaed1dac07b266ca2c662ff4a184a3060f upstream.
    
    The patch fixes two issues in the error path cleanup:
    - in MUSB_PORT_MODE_DUAL_ROLE mode, if musb_gadget_setup() fails we
      never cleanup the host struct earlier allocated.
    - if musb_init_debugfs() or sysfs_create_group() fails, then we never
      free the host part initialization, only device part.
    
    Cc: Daniel Mack <zonque@gmail.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Luis Henriques <luis.henriques@canonical.com>
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -1962,6 +1962,8 @@ musb_init_controller(struct device *dev, int nIrq, void __iomem *ctrl)
 		if (status < 0)
 			goto fail3;
 		status = musb_gadget_setup(musb);
+		if (status)
+			musb_host_cleanup(musb);
 		break;
 	default:
 		dev_err(dev, "unsupported port mode %d\n", musb->port_mode);
@@ -1988,6 +1990,7 @@ fail5:
 
 fail4:
 	musb_gadget_cleanup(musb);
+	musb_host_cleanup(musb);
 
 fail3:
 	pm_runtime_put_sync(musb->controller);
